5c8083471a8fa9945fdb957743c641cadfa1867e
junh0y <y.junh0y@gmail.com>
2024-05-10 02:12:11 +0900

    초기화

--- src/components/Row.js
+++ src/components/Row.js
@@ -71,9 +71,8 @@ export default function Row({ isLargeRow, title, id, fetchUrl }) {
             >
                 <div id={id} className="row__posters">
                     {movies.map((movie) => (
-                        <SwiperSlide>
+                        <SwiperSlide key={movie.id}>
                             <img
-                                key={movie.id}
                                 className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                                 src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
                                 alt={movie.name}
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/ReactNetflixClone">
+  <BrowserRouter basename='{process.env.PROJECT_URL}'>
     <App />
   </BrowserRouter>
 );
